ed3bc681b6c664fa796f10b12786481a09074262
JuanJo <juanjofer1999@gmail.com>
2020-03-06 09:23:50 -0400

    lis selector implementado

--- main/java/com/example/listviewpersonalizado/MainActivity.java
+++ main/java/com/example/listviewpersonalizado/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.listviewpersonalizado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         adaptador = new Adaptador(this,lista);
 
         lvData.setAdapter(adaptador);
+
+        lvData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getApplicationContext(),lista.get(position).getTitulo(),Toast.LENGTH_LONG).show();
+//                LinearLayout llitem;
+//                llitem=(LinearLayout)view.findViewById(R.id.llItem);
+//                llitem.setBackgroundColor(0xfff44336);
+            }
+        });
         cargaLista();
     }
 
